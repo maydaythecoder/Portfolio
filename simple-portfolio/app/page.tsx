f4b87307b3ad552eb490bc35f6286b3ef807e14a
mayday <maydaythecoder@gmail.com>
2024-05-17 14:50:49 -0400

    added todos, and removed white space

--- simple-portfolio/app/page.tsx
+++ simple-portfolio/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
+
+//TODO: modularize file into components
+//TODO: add spacing between images
+//TODO: move Image-Track-Two down half way
+//TODO: add scroll effect with bezier curve
+//TODO: add button at center of image on hover to link to project
 
 export default function Home() {
   return (
@@ -110,7 +115,6 @@ export default function Home() {
         </div>
 
         {/* Footer */}
-
         <div className=" bg-black h-[60vh] fixed bottom-0 left-0 w-full -z-50">
           <h5 className="text-[white] text-center pt-24 font-space-grotesk font-normal tracking-[-0.025em] text-8xl leading-[1em] md:text-[100px] text-[color:var(--64ddf9a6011f1)]">
             Let's work together
