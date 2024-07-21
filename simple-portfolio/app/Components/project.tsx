a7c2193ff9ff7b33c60c205459f08feae25cb8e2
mayday <maydaythecoder@gmail.com>
2024-07-21 18:47:44 -0400

    updated next.config.mjs

---
+++ simple-portfolio/app/Components/project.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer'
+import Images from './Projects'
+import Header from './header'
+
+const project = () => {
+  return (
+    <div className='z-50>
+                  <Header />
+            <Images />
+            <Footer />
+    </div>
+  )
+}
+
+export default project
--- simple-portfolio/next.config.mjs
+++ simple-portfolio/next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
     images: {
         domains: [
           'skillicons.dev',
+          'user-images.githubusercontent.com',
+          'techstack-generator.vercel.app',
           'fontawesome.com'
         ],
       },
