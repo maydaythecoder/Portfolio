257fc0edc4bb6772b746e780e25a416a67216a2d
mayday <maydaythecoder@gmail.com>
2024-07-21 16:09:04 -0400

    added bg

--- simple-portfolio/app/page.tsx
+++ simple-portfolio/app/page.tsx
@@ -10,7 +10,30 @@ import Footer from "./Components/Footer";
 export default function Home() {
   return (
     <div className="relative">
-      <div className="bg-white p-4 sm:p-6 md:p-10 mb-20 md:mb-40 lg:mb-80 align-bottom h-full rounded-b-3xl">
+                  <svg
+              className="absolute inset-0 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+              aria-hidden="true"
+            >
+              <defs>
+                <pattern
+                  id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
+                  width={200}
+                  height={200}
+                  x="100%"
+                  y={-1}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <path d="M130 200V.5M.5 .5H200" fill="none" />
+                </pattern>
+              </defs>
+              <rect width="100%" height="100%" strokeWidth={0} fill="white" />
+              <svg x="100%" y={-1} className="overflow-visible fill-gray-50">
+                <path d="M-470.5 0h201v201h-201Z" strokeWidth={0} />
+              </svg>
+              <rect width="100%" height="100%" strokeWidth={0} fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
+            </svg>
+      <div className="bg-slate-100 p-4 sm:p-6 md:p-10 mb-20 md:mb-40 lg:mb-80 align-bottom h-full rounded-b-3xl">
+        
         <Header />
         <Images />
       </div>
